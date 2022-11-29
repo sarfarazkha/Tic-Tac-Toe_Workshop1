e549a545cce449e57db4551d24e4fa75a1583116
sarfarazkha <102785717+sarfarazkha@users.noreply.github.com>
2022-11-29 20:02:20 +0530

    Tic-Toc-Toe Uc1: creating a board

---
+++ src/com/bridgelabz/TikTakToe1.java
@@ -0,0 +1,17 @@
+package com.bridgelabz;
+
+public class TikTakToe1 {
+    static void board() {
+        char[] arr = new char[10];
+        for (int i = 1; i < 9; i++) {
+            arr[i] = ' ';
+
+        }
+    }
+
+    public static void main(String[] args) {
+        board();
+
+    }
+
+}
